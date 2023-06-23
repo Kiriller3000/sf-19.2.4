4a45368ed70cf3d1f1a30cf3c544540351573019
Kirill <134620940+Kiriller3000@users.noreply.github.com>
2023-06-23 15:56:31 +0500

    Update test_calc.py

--- Calc/tests/test_calc.py
+++ Calc/tests/test_calc.py
@@ -1,21 +1,21 @@
-import pytest
-from app.calculator import Calculator
+# import pytest
+from app.calc import Calculator
 
 class TestCalc:
     def setup(self):
         self.calc = Calculator
 
     def test_adding(self):
-        self.calc.adding(self, 1, 1) == 2
+        assert self.calc.adding(self, 1, 1) == 2
 
     def test_subtraction(self):
-        self.calc.subtraction(self, 2, 1) == 1
+        assert self.calc.subtraction(self, 2, 1) == 1
 
     def test_multiply(self):
-        self.calc.multiply(self, 2, 3) == 6
+        assert self.calc.multiply(self, 2, 3) == 6
 
     def test_division(self):
-        self.calc.division(self, 6, 3) == 2
+        assert self.calc.division(self, 6, 3) == 2
 
     def teardown(self):
         print('Выполнение метода Teardown')
